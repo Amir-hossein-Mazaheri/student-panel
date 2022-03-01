b662a52c3ac798c8407122064d1adb4bcff904df
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-01 20:02:42 +0330

    firts initlaize

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    "postcss-css-variables": {}, // replace vars with their value
+    autoprefixer: {},
+    "postcss-combine-media-query": {}, // combine media queries with the same screen value
+  },
+};
